9acba454534d96b716948e1be7b3e271c0abc1db
Carlo <carlolazzari4@gmail.com>
2020-11-07 13:19:15 +0100

    Update MagazzinoDAO.java

--- src/model/dao/MagazzinoDAO.java
+++ src/model/dao/MagazzinoDAO.java
@@ -1,11 +1,16 @@
 package model.dao;
 
+import model.dao.exception.DuplicatedObjectException;
+import model.mo.CentroVendita;
 import model.mo.Magazzino;
 
 import java.util.ArrayList;
 
 public interface MagazzinoDAO {
 
+    public Magazzino create(CentroVendita centroVendita, String nomeMagazzino, String indirizzo) throws DuplicatedObjectException;
+    public void update(Magazzino magazzino) throws DuplicatedObjectException;
+    public void delete(Magazzino magazzino);
     public Magazzino findByNomeMagazzino(String nome_magazzino);
     public ArrayList<Magazzino> findAllMagazzini();
 
