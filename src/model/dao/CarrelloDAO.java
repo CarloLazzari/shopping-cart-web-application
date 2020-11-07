a0f03a73220a1af34ff16ff0499649c3160c5255
Carlo <carlolazzari4@gmail.com>
2020-11-07 13:21:10 +0100

    Update CarrelloDAO.java

--- src/model/dao/CarrelloDAO.java
+++ src/model/dao/CarrelloDAO.java
@@ -1,22 +1,21 @@
-    package model.dao;
+package model.dao;
 
-    import model.mo.Carrello;
-    import model.mo.Fumetto;
-    import model.mo.User;
+import model.mo.Carrello;
+import model.mo.Fumetto;
+import model.mo.User;
 
-    import java.util.ArrayList;
+import java.util.ArrayList;
 
-    public interface CarrelloDAO {
+public interface CarrelloDAO {
 
-        public Carrello addToCart(Fumetto fumetto, User user);
-        public Carrello findByISBNAndUsername(String ISBNProdotto, String username);
-        public ArrayList<Carrello> viewCart(String username);
-        public void removeFromCart(Carrello carrello);
-        public void addQuantity(Carrello carrello);
-        public void substractQuantity(Carrello carrello);
-        public void flushCart(String username);
-        public float calculatePrice(String username);
-        public int getQuantity(String username, String ISBN);
+    public Carrello addToCart(Fumetto fumetto, User user);
+    public Carrello findByISBNAndUsername(String ISBNProdotto, String username);
+    public ArrayList<Carrello> viewCart(String username);
+    public void removeFromCart(Carrello carrello);
+    public void addQuantity(Carrello carrello);
+    public void substractQuantity(Carrello carrello);
+    public void flushCart(String username);
+    public float calculatePrice(String username);
+    public int getQuantity(String username, String ISBN);
 
-
-    }
+}
