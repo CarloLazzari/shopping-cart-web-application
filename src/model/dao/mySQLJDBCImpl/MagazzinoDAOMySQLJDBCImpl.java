e1c8965dd70696579cc3b9ed48cfc40e08ddc96f
Carlo <carlolazzari4@gmail.com>
2020-11-07 13:15:17 +0100

    Update MagazzinoDAOMySQLJDBCImpl.java

--- src/model/dao/mySQLJDBCImpl/MagazzinoDAOMySQLJDBCImpl.java
+++ src/model/dao/mySQLJDBCImpl/MagazzinoDAOMySQLJDBCImpl.java
@@ -1,6 +1,7 @@
 package model.dao.mySQLJDBCImpl;
 
 import model.dao.MagazzinoDAO;
+import model.dao.exception.DuplicatedObjectException;
 import model.mo.CentroVendita;
 import model.mo.Magazzino;
 import java.sql.Connection;
@@ -15,9 +16,108 @@ public class MagazzinoDAOMySQLJDBCImpl implements MagazzinoDAO {
 
     public MagazzinoDAOMySQLJDBCImpl(Connection conn){ this.conn = conn;}
 
-    /* Per l'admin */
     @Override
-    public Magazzino findByNomeMagazzino(String nome_magazzino){
+    public Magazzino create(CentroVendita centroVendita, String nomeMagazzino, String indirizzo) throws DuplicatedObjectException {
+
+        PreparedStatement ps;
+
+        Magazzino magazzino = new Magazzino();
+        magazzino.setCentroVendita(centroVendita);
+        magazzino.setNomeMagazzino(nomeMagazzino);
+        magazzino.setIndirizzo(indirizzo);
+
+        try {
+            String sql
+                    = " SELECT NOME_MAGAZZINO "
+                    + " FROM magazzino "
+                    + " WHERE NOME_MAGAZZINO = ?";
+
+            ps = conn.prepareStatement(sql);
+            int i = 1;
+            ps.setString(i, magazzino.getNomeMagazzino());
+
+            ResultSet resultSet = ps.executeQuery();
+
+            boolean exists;
+            exists=resultSet.next();
+            resultSet.close();
+
+            if(exists) {
+                throw new DuplicatedObjectException("MagazzinoDAOMySQLJDCBImpl: Tentativo di inserimento di un magazzino già esistente");
+            }
+
+            sql
+                = " INSERT INTO magazzino"
+                + " (NOME_CENTRO_APPARTENENZA, INDIRIZZO, NOME_MAGAZZINO) "
+                + " VALUES (?,?,?)";
+
+            ps = conn.prepareStatement(sql);
+            i = 1;
+            ps.setString(i++, magazzino.getCentroVendita().getNomeCentro());
+            ps.setString(i++, magazzino.getIndirizzo());
+            ps.setString(i, magazzino.getNomeMagazzino());
+
+            ps.executeUpdate();
+            ps.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
+
+        return magazzino;
+    }
+
+    @Override
+    public void update(Magazzino magazzino) throws DuplicatedObjectException {
+
+        PreparedStatement ps;
+
+        try {
+            String sql
+                    = " SELECT NOME_MAGAZZINO"
+                    + " FROM magazzino"
+                    + " WHERE NOME_MAGAZZINO <> ?"
+                    + " AND INDIRIZZO = ?"
+                    + " AND NOME_CENTRO_APPARTENENZA = ?";
+
+            ps = conn.prepareStatement(sql);
+            int i = 1;
+            ps.setString(i++, magazzino.getNomeMagazzino());
+            ps.setString(i++, magazzino.getIndirizzo());
+            ps.setString(i, magazzino.getCentroVendita().getNomeCentro());
+
+            ResultSet resultSet = ps.executeQuery();
+
+            boolean exists;
+            exists = resultSet.next();
+            resultSet.close();
+
+            if (exists) {
+                throw new DuplicatedObjectException("MagazzinoDAOMySQLJDCBImpl: Tentativo di inserimento di magazzino già esistente");
+            }
+
+            sql
+                = " UPDATE magazzino "
+                + " SET "
+                + " INDIRIZZO = ?"
+                + " WHERE NOME_MAGAZZINO = ?";
+
+            ps = conn.prepareStatement(sql);
+            i = 1;
+            ps.setString(i++, magazzino.getIndirizzo());
+            ps.setString(i, magazzino.getNomeMagazzino());
+
+            ps.executeUpdate();
+            ps.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
+
+    }
+
+    @Override
+    public Magazzino findByNomeMagazzino(String nomeMagazzino){
 
         PreparedStatement ps;
         Magazzino magazzino = null;
@@ -30,7 +130,8 @@ public class MagazzinoDAOMySQLJDBCImpl implements MagazzinoDAO {
                     + " NOME_MAGAZZINO = ? ";
 
             ps = conn.prepareStatement(sql);
-            ps.setString(1, nome_magazzino);
+            int i = 1;
+            ps.setString(i, nomeMagazzino);
 
             ResultSet resultSet = ps.executeQuery();
 
@@ -47,7 +148,6 @@ public class MagazzinoDAOMySQLJDBCImpl implements MagazzinoDAO {
         return magazzino;
     }
 
-    /* Per l'admin */
     @Override
     public ArrayList<Magazzino> findAllMagazzini() {
 
@@ -78,6 +178,11 @@ public class MagazzinoDAOMySQLJDBCImpl implements MagazzinoDAO {
         return magazzini;
     }
 
+    @Override
+    public void delete(Magazzino magazzino) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
     Magazzino read(ResultSet rs) throws SQLException {
 
         Magazzino magazzino = new Magazzino();
@@ -85,20 +190,20 @@ public class MagazzinoDAOMySQLJDBCImpl implements MagazzinoDAO {
         magazzino.setCentroVendita(centroVendita);
 
         try {
-            magazzino.getCentroVendita().setNomecentro(rs.getString("NOME_CENTRO_REF"));
-        } catch (SQLException e) {
+            magazzino.getCentroVendita().setNomecentro(rs.getString("NOME_CENTRO_APPARTENENZA"));
+        } catch (SQLException ignored) {
         }
         try {
             magazzino.setIndirizzo(rs.getString("INDIRIZZO"));
-        } catch (SQLException e) {
+        } catch (SQLException ignored) {
         }
         try{
             magazzino.setNomeMagazzino(rs.getString("NOME_MAGAZZINO"));
         }
-        catch (SQLException e) {
+        catch (SQLException ignored) {
         }
 
         return magazzino;
-
+        
     }
 }
