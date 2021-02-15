903ba884e9d22a2d2d5f5ee262dbd57db7b2c7a6
Mikgazer <carlolazzari4@gmail.com>
2021-02-15 16:30:39 +0100

    Update ConfirmOrderManagement.java

--- src/controller/ConfirmOrderManagement.java
+++ src/controller/ConfirmOrderManagement.java
@@ -15,7 +15,6 @@ import java.util.logging.Logger;
 
 public class ConfirmOrderManagement {
 
-
     private ConfirmOrderManagement() {}
 
     public static void view(HttpServletRequest request, HttpServletResponse response){
